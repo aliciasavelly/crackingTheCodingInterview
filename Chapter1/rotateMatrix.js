1024823790420b9b6937338e41a04bac8c591842
aliciasavelly <alicia.savelly@gmail.com>
2017-06-19 15:38:19 -0700

    finish rotate matrix

--- Chapter1/rotateMatrix.js
+++ Chapter1/rotateMatrix.js
@@ -13,6 +13,12 @@ function rotateMatrix(matrix) {
     result.push([]);
   }
 
+  for (let j = 0; j < matrix.length; j++) {
+    for (let k = 0; k < matrix[0].length; k++) {
+      result[k].unshift(matrix[j][k]);
+    }
+  }
+
   return result;
 }
 
@@ -20,3 +26,10 @@ console.log(rotateMatrix([["A", "B", "C", "D"],
                           ["E", "F", "G", "H"],
                           ["I", "J", "K", "L"],
                           ["M", "N", "O", "P"]]));
+
+console.log(rotateMatrix([["A", "B"],
+                          ["C", "D"]]));
+
+console.log(rotateMatrix([["A", "B"],
+                          ["C", "D"],
+                          ["E", "F"]]));
