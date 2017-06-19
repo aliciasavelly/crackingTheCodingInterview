1329a24b4a109e014687978eaec564ada4f95702
aliciasavelly <alicia.savelly@gmail.com>
2017-06-19 13:29:34 -0700

    add descriptions and refactor one away

--- Chapter1/checkPermutation.js
+++ Chapter1/checkPermutation.js
@@ -1,3 +1,10 @@
+/*
+
+Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+
+*/
+
+
 // function checkPermutation(str1, str2) {
 //   if (str1.length != str2.length) {
 //     return false;
--- Chapter1/oneAway.js
+++ Chapter1/oneAway.js
@@ -1,3 +1,11 @@
+/*
+
+One Away: There are three types of edits that can be performed on strings:
+insert a character, remove a character, or replace a character. Given two
+strings, write a function to check if they are one edit (or zero edits) away.
+
+*/
+
 function oneAway(str1, str2) {
   if (str2.length < str1.length - 1 || str2.length > str1.length + 1) {
     return false;
@@ -5,20 +13,18 @@ function oneAway(str1, str2) {
     return true;
   }
 
-  let edit = false;
   let i = 0;
   let j = 0;
   while (i < str1.length || j < str2.length) {
-    if (str1[i] != str2[j] && edit == true) {
-      return false;
-    } else if (str1[i] != str2[j]) {
-      edit = true;
+    if (str1[i] != str2[j]) {
       if (str1.length > str2.length) {
         j--;
       } else if (str1.length < str2.length) {
         i--;
       }
+      return str1.slice(i + 1, str1.length) === str2.slice(j + 1, str2.length);
     }
+    
     i++;
     j++;
   }
