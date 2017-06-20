bfd186421a16cab5e3922b25ffcd1bd7ec211682
aliciasavelly <alicia.savelly@gmail.com>
2017-06-20 16:56:28 -0700

    add intersection

--- Chapter2/intersection.js
+++ Chapter2/intersection.js
@@ -1,4 +1,5 @@
 const SinglyLinkedList = require('./singlyLinkedList.js');
+const SingleLink = require('./singleLink.js');
 
 /*
 
@@ -11,5 +12,46 @@ then they are intersecting.
 */
 
 function intersection(list1, list2) {
+  if (list1.head == list2.head) {
+    return true;
+  }
 
+  let current = list1.head;
+
+  while (current) {
+    let temp = current.next;
+    current.next = 0;
+    current = temp;
+  }
+
+  let current = list2.head;
+
+  while (current) {
+    if (current.next == 0) {
+      return true;
+    }
+  }
+
+  return false;
 }
+
+let ll1 = new SinglyLinkedList();
+let ll2 = new SinglyLinkedList();
+
+let l1 = new SingleLink(1);
+let l2 = new SingleLink(2);
+let l3 = new SingleLink(3);
+let l4 = new SingleLink(4);
+let l5 = new SingleLink(5);
+let l6 = new SingleLink(6);
+let l7 = new SingleLink(7);
+
+ll1.append(0);
+ll1.head.next = l1;
+l1.next = l2;
+l2.next = l3;
+
+ll2.append(0);
+ll2.head.next = l4;
+l4.next = l5;
+l5.next = l2;
