be8b0399a59620f3c0e8615f13abe31b7fd8171d
aliciasavelly <alicia.savelly@gmail.com>
2017-06-19 17:25:28 -0700

    work on link removal

--- Chapter2/link.js
+++ Chapter2/link.js
@@ -7,12 +7,22 @@ class Link {
 
   remove() {
     if (this.prev != null) {
+      console.log(this.prev.next);
       this.prev.next = this.next;
     }
     if (this.next != null) {
       this.next.prev = this.prev;
     }
+    this.next = null;
+    this.prev = null;
   }
 }
+
+let l = new Link(3);
+console.log(l);
+let l2 = new Link(2);
+l.next = l2;
+l.next.prev = l;
+console.log(l);
 
 module.exports = Link;
--- Chapter2/removeDups.js
+++ Chapter2/removeDups.js
@@ -11,8 +11,37 @@ ll.append(1);
 ll.append(9);
 // 1, 2, 3, 4, 9
 
-function removeDups(ll) {
+function removeDups(linkedList) {
   let valHash = {};
+  let current = linkedList.head.next;
 
-  
+  while (current.next) {
+    if (valHash[current.value]) {
+      let toRemove = current;
+      current = current.next;
+      console.log(toRemove);
+      toRemove.remove();
+    } else {
+      valHash[current.value] = true;
+    }
+  }
+
+  return linkedList;
 }
+
+console.log(ll.head.next.value);
+console.log(ll.head.next.next.value);
+console.log(ll.head.next.next.next.value);
+console.log(ll.head.next.next.next.next.value);
+console.log(ll.head.next.next.next.next.next.value);
+console.log(ll.head.next.next.next.next.next.next.value);
+console.log(ll.head.next.next.next.next.next.next.next.value);
+console.log(ll.head.next.next.next.next.next.next.next.next.value);
+removeDups(ll);
+console.log("XXXXXXXXXX");
+ll.head.next.remove();
+console.log(ll.head.next.value);
+console.log(ll.head.next.next.value);
+console.log(ll.head.next.next.next.value);
+console.log(ll.head.next.next.next.next.value);
+console.log(ll.head.next.next.next.next.next.value);
