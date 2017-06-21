e995b5fdf312f6656bb5219c5e0047a7f85015b4
aliciasavelly <alicia.savelly@gmail.com>
2017-06-20 20:15:00 -0700

    add queue

---
+++ Chapter3/queue.js
@@ -0,0 +1,22 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  add(item) {
+    this.queue.push(item);
+    return item;
+  }
+
+  remove() {
+    return this.queue.unshift();
+  }
+
+  peek() {
+    return this.queue[0];
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+}
