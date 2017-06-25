84a8492b72884df672aaf71b5bcfbd4c0f1fa39e
aliciasavelly <alicia.savelly@gmail.com>
2017-06-25 12:32:52 -0700

    clean up code

--- Chapter4/BST.js
+++ Chapter4/BST.js
@@ -29,27 +29,23 @@ class BST {
     let node = new BSTNode(value);
 
     if (!root.value) {
-      console.log("32");
       root.value = node.value;
+
     } else if (value <= root.value) {
-      console.log("35");
+
       if (root.left) {
-        console.log("37");
         let left_tree = new BST(root.left);
         left_tree.insert(value);
-        console.log(left_tree);
       } else {
-        console.log("41");
         root.left = node;
       }
+
     } else if (value > root.value) {
-      console.log("45");
+
       if (root.right) {
-        console.log("47");
         let right_tree = new BST(root.right);
         right_tree.insert(value);
       } else {
-        console.log("51");
         root.right = node;
       }
     }
