b8dcfb10ee8b175fa920b463a6b8f80ec3657d16
aliciasavelly <alicia.savelly@gmail.com>
2017-06-26 18:49:31 -0700

    add successor

---
+++ Chapter4/successor.js
@@ -0,0 +1,29 @@
+const BST = require('./BST.js');
+const BSTNode = require('./BSTNode.js');
+
+/*
+
+Successor: Write an algorithm to find the "next" node. (i.e., in-order
+successor) of a given node in a binary search tree. You may assume that each
+node has a link to its parent.
+
+*/
+
+function successor(node) {
+
+}
+
+let val = new BSTNode(10);
+let tree = new BST(val);
+tree.insert(3);
+tree.insert(7);
+tree.insert(8);
+tree.insert(2);
+tree.insert(1);
+tree.insert(4);
+tree.insert(15);
+tree.insert(17);
+tree.insert(20);
+tree.insert(23);
+tree.insert(11);
+tree.insert(16);
