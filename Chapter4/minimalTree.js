ad0b7a7f8dd3166d17535e948e72bd98d6f00303
aliciasavelly <alicia.savelly@gmail.com>
2017-06-26 18:03:42 -0700

    remove unnecessary lines

--- Chapter4/minimalTree.js
+++ Chapter4/minimalTree.js
@@ -39,38 +39,15 @@ function addToTree(arr, root) {
       newNode.left = new BSTNode(arr[0]);
       newNode.right = new BSTNode(arr[2]);
     }
-  // } else if (arr.length == 1) {
-  //   if (arr[0] <= root.value) {
-  //     root.left = new BSTNode(arr[0]);
-  //   } else {
-  //     root.right = new BSTNode(arr[0]);
-  //   }
-  // } else if (arr.length == 3) {
-  //   if (arr[0] <= root.value) {
-  //     let newNode = new BSTNode(arr[1]);
-  //     root.left = newNode;
-  //   } else {
-  //     let newNode = new BSTNode(arr[1]);
-  //     root.right = newNode;
-  //   }
-  //   newNode.left = new BSTNode(arr[0]);
-  //   newNode.right = new BSTNode(arr[2]);
   } else {
-    console.log("59");
     let midIdx = Math.floor(arr.length / 2);
     let newRoot = new BSTNode(arr[midIdx]);
-    let resultRoot;
+    let resultRoot = newRoot;
 
     if (newRoot.value <= root.value) {
-      console.log(64);
       root.left = newRoot;
-      resultRoot = root.left;
     } else {
-      console.log(68);
-      console.log(newRoot);
       root.right = newRoot;
-      resultRoot = root.right;
-      console.log(resultRoot);
     }
 
     addToTree(arr.slice(0, midIdx), resultRoot);
@@ -78,5 +55,5 @@ function addToTree(arr, root) {
   }
 }
 
-// let tree = minimalTree([1, 2, 3, 4, 5, 6, 7]);
+let tree = minimalTree([1, 2, 3, 4, 5, 6, 7]);
 let tree2 = minimalTree([1, 2, 3, 4, 5, 6, 7, 8]);
