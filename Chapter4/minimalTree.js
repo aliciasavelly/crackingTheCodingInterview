ebfe19b62206f56933cbd77b61227c3ebe86a4dc
aliciasavelly <alicia.savelly@gmail.com>
2017-06-26 18:02:04 -0700

    fix minimal tree code for even length arrays

--- Chapter4/minimalTree.js
+++ Chapter4/minimalTree.js
@@ -26,7 +26,6 @@ function addToTree(arr, root) {
       root.right = new BSTNode(arr[0]);
       root.right.right = new BSTNode(arr[1]);
     }
-
   } else if (arr.length == 1 || arr.length == 3) {
     let newNode = (arr.length == 1 ? new BSTNode(arr[0]) : new BSTNode(arr[1]));
 
@@ -40,17 +39,38 @@ function addToTree(arr, root) {
       newNode.left = new BSTNode(arr[0]);
       newNode.right = new BSTNode(arr[2]);
     }
+  // } else if (arr.length == 1) {
+  //   if (arr[0] <= root.value) {
+  //     root.left = new BSTNode(arr[0]);
+  //   } else {
+  //     root.right = new BSTNode(arr[0]);
+  //   }
+  // } else if (arr.length == 3) {
+  //   if (arr[0] <= root.value) {
+  //     let newNode = new BSTNode(arr[1]);
+  //     root.left = newNode;
+  //   } else {
+  //     let newNode = new BSTNode(arr[1]);
+  //     root.right = newNode;
+  //   }
+  //   newNode.left = new BSTNode(arr[0]);
+  //   newNode.right = new BSTNode(arr[2]);
   } else {
-
+    console.log("59");
     let midIdx = Math.floor(arr.length / 2);
     let newRoot = new BSTNode(arr[midIdx]);
+    let resultRoot;
 
-    if (newRoot < root.value) {
+    if (newRoot.value <= root.value) {
+      console.log(64);
       root.left = newRoot;
-      let resultRoot = root.left;
+      resultRoot = root.left;
     } else {
+      console.log(68);
+      console.log(newRoot);
       root.right = newRoot;
-      let resultRoot = root.right;
+      resultRoot = root.right;
+      console.log(resultRoot);
     }
 
     addToTree(arr.slice(0, midIdx), resultRoot);
@@ -58,5 +78,5 @@ function addToTree(arr, root) {
   }
 }
 
-let tree = minimalTree([1, 2, 3, 4, 5, 6, 7]);
+// let tree = minimalTree([1, 2, 3, 4, 5, 6, 7]);
 let tree2 = minimalTree([1, 2, 3, 4, 5, 6, 7, 8]);
